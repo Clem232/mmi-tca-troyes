313f03f383a70139241a2599639e0346784b4ac3
Clem232 <L'ADRESSE EMAIL DE VOTRE COMPTE GIT>
2025-11-04 14:18:27 +0100

    Ajout du workflow CI/CD et des ressources associées

--- ressources/s5/klement-rr/projets/ci-cd-symfony/src/Service/DiscountCalculator.php
+++ ressources/s5/klement-rr/projets/ci-cd-symfony/src/Service/DiscountCalculator.php
@@ -4,10 +4,11 @@ namespace App\Service;
 
 class DiscountCalculator
 {
-public function calculateDiscount(float $totalAmount, bool $isVipCustomer): float
+    public function calculateDiscount(float $totalAmount, bool $isVipCustomer): float
     {
         // Remise de base de 10% si le montant est supérieur à 100 €
         $discount = ($totalAmount > 100) ? $totalAmount * 0.10 : 0;
+
         // Remise supplémentaire de 5% pour les clients VIP
         if ($isVipCustomer) {
             $discount += $totalAmount * 0.05;
